5c171c4a2badeaf3f2686b69631a3735ea989afe
MonteJuanppy <62229811+MonteJuanppy@users.noreply.github.com>
2021-11-18 01:48:06 -0600

    Se modifica el archivo L4
    Se modifica el archivo de tal forma que sea la solución al problema 4 del la práctica E13

--- L4.py
+++ L4.py
@@ -1,29 +1,37 @@
-# Base para la solución del Laboratorio 4
-
-# Los parámetros T, t_final y N son elegidos arbitrariamente
-
+#Juan Ignacio Montealegre Salazar
+#Carné B95001
+#Laboratorio 4 de Modelos Probabilísticos de Señales y Sistemas
+#Código base modificado para solucionar problema 4 de de la práctica E13
+'''
+Parte a) Suponiendo que Ω no es una variable aleatoria, 
+sino una constante omega. Encuentre el valor medio
+ Los parámetros T, t_final y N son elegidos arbitrariamente
+'''
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
 # Variables aleatorias A y Z
-vaA = stats.norm(3, np.sqrt(10))
-vaZ = stats.uniform(-np.pi/2, np.pi)
+vaA = stats.norm(5, np.sqrt(0.2))
+vaZ = stats.uniform(0, np.pi/2)
+
+#Constante Omega
+Omega = 2*np.pi
 
 # Creación del vector de tiempo
 T = 100			# número de elementos
-t_final = 10	# tiempo en segundos
+t_final = 5# tiempo en segundos
 t = np.linspace(0, t_final, T)
 
 # Inicialización del proceso aleatorio X(t) con N realizaciones
-N = 10
+N = 20
 X_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
 
 # Creación de las muestras del proceso x(t) (A y Z independientes)
 for i in range(N):
 	A = vaA.rvs()
 	Z = vaZ.rvs()
-	x_t = A * np.cos(np.pi*t + Z)
+	x_t = A * np.cos(Omega*t + Z)
 	X_t[i,:] = x_t
 	plt.plot(t, x_t)
 
@@ -32,7 +40,9 @@ P = [np.mean(X_t[:,i]) for i in range(len(t))]
 plt.plot(t, P, lw=6)
 
 # Graficar el resultado teórico del valor esperado
-E = 6/np.pi * np.cos(np.pi*t)
+E = (10/np.pi) * (np.cos(Omega*t)-np.sin(Omega*t))
+print ("Valor teórico de la media E: ")
+print(E)
 plt.plot(t, E, '-.', lw=4)
 
 # Mostrar las realizaciones, y su promedio calculado y teórico
@@ -41,6 +51,9 @@ plt.xlabel('$t$')
 plt.ylabel('$x_i(t)$')
 plt.show()
 
+'''Parte b) Suponga ahora que Ω y Θ son constantes, no variables aleatorias
+Obtenga el valor teórico de la correlación'''
+Z = 0
 # T valores de desplazamiento tau
 desplazamiento = np.arange(T)
 taus = desplazamiento/t_final
@@ -58,7 +71,9 @@ for n in range(N):
 	plt.plot(taus, corr[n,:])
 
 # Valor teórico de correlación
-Rxx = 19/2 * np.cos(np.pi*taus)
+Rxx = 22.5 * np.cos(Omega*t+Z) * np.cos(Omega*(t+taus)+Z)
+print ("Valor teórico de la correlación Rxx: ")
+print(Rxx)
 
 # Gráficas de correlación para cada realización y la
 plt.plot(taus, Rxx, '-.', lw=4, label='Correlación teórica')
@@ -67,3 +82,4 @@ plt.xlabel(r'$\tau$')
 plt.ylabel(r'$R_{XX}(\tau)$')
 plt.legend()
 plt.show()
+
